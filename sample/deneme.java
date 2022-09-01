068f17e5cd1f126d5cdb287491f08956eda17372
Beril <50303910+berilerzurum@users.noreply.github.com>
2022-09-01 16:12:59 +0300

    Docker ilk örnek

---
+++ sample/deneme.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class deneme {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int a = r.nextInt(10);
+        System.out.println("Rastgele sayi:" + a);
+    }
+}
