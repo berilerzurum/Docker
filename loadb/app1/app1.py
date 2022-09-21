25184e35219c7573ddf528212ead30483dfd4873
Beril <50303910+berilerzurum@users.noreply.github.com>
2022-09-21 15:04:22 +0300

    Load Balancing-Nginx/Docker

---
+++ loadb/app1/app1.py
@@ -0,0 +1,13 @@
+
+from flask import request, Flask
+import json
+
+app1 = Flask(__name__)
+@app1.route('/')
+
+def hello_world():
+  
+  return 'this is response from app1'
+
+if __name__ == '__main__':
+  app1.run(debug=True, host='0.0.0.0')
---
+++ loadb/app2/app2.py
@@ -0,0 +1,14 @@
+from flask import request, Flask
+import json
+
+app2 = Flask(__name__)
+@app2.route('/')
+
+def hello_world():
+  
+  return 'this is response from app2'
+
+if __name__ == '__main__':
+  app2.run(debug=True, host='0.0.0.0')
+ 
+#5000 portundan cevap verecek.
